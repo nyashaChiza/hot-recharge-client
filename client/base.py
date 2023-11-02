0a35f39438cee43481c00fe7e08552d58a5cbc90
NyashaChiza <nchizampeni@gmail.com>
2023-11-02 10:36:53 +0200

    savepoint

---
+++ client/base.py
@@ -0,0 +1,76 @@
+import os
+import loguru
+import hotrecharge
+import random
+import json
+import requests
+from decouple import config
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(ROOT_DIR, '.env')
+logger = loguru.logger
+
+CUSTOM_MESSAGE = os.getenv('CUSTOM_MESSAGE')
+
+class Client():
+    def __init__(self, access_code, access_password):
+        self.session = requests.Session()
+        self.session.headers={
+            'x-access-code': access_code,
+            'x-access-password': access_password,
+            'x-agent-reference': f"airtym-{str(random.randint(0,10000000))}",
+            "content-type": 'application/json',
+            "cache-control": "no-cache",
+        }
+        
+
+    def __str__(self):
+        return f'{self.access_code}'  
+                
+    def reset_reference(self):
+        self.session.headers['x-agent-reference'] = f'aitym-{str(random.randint(0,100))}{str(random.randint(0,10000))}'
+
+
+    def get_evds(self)->object:
+        evds = self.self.api.getEVDs()
+        return evds
+    
+    def get_wallet_balance(self)->object:
+        self.reset_reference()
+        self.reset_reference()
+        url = 'https://ssl.hot.co.zw/api/v1/agents/wallet-balance'
+        
+        response = self.session.get(url)
+       
+        return response.json()['WalletBalance']
+    
+    def get_data_bundles(self)->object:
+        self.reset_reference()
+        url ='https://ssl.hot.co.zw/api/v1/agents/get-data-bundles-all'
+        response = self.session.get(url)
+       
+        return response
+            
+    def recharge_data_bundles(self, data_bundle_code:str,recipient_phone_number:str)->object:
+        self.reset_reference()
+        custom_message = f'Your account has been topped with {data_bundle_code} data bundle .\n Thank you for doing business with Airtym.com'
+        headers = {
+            'x-access-password': self.access_password,
+            'x-access-code': self.access_code
+        }
+        recharge_data_bundle_response = self.api.dataBundleRecharge(product_code=data_bundle_code, number=recipient_phone_number, mesg=custom_message, headers=headers)
+        return recharge_data_bundle_response
+
+    def recharge_airtime(self, amount:int, recipient_phone_number:str)->object:
+        self.reset_reference()
+        url = "https://ssl.hot.co.zw/api/v1/agents/recharge-pinless"
+        custom_message = f'Your account has been topped with {amount} RTGS .\n Thank you for doing business with Airtym.com'
+        body ={"amount": amount,"targetMobile": recipient_phone_number,"CustomerSMS":custom_message}
+        
+        response = self.session.post(url, json=body)
+        return response.json()
+    
+    
+    def recharge_zesa(self, meter_number:str, amount:int)->object:
+         #zesa logic here
+        return None
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import loguru
-from HTTPclient.client import Client
-from dotenv import load_dotenv
+from client import Client
+from decouple import config
 import os
 
 logger = loguru.logger
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 dotenv_path = os.path.join(ROOT_DIR, '.env')
-load_dotenv(dotenv_path)
+
 
 #example call to client method
 
-access_code=os.getenv('ACC_EMAIL_ADDRESS')
-access_password=os.getenv('ACC_PASSWORD')
-reference=os.getenv('REFERENCES')
+access_code=config('ACC_EMAIL_ADDRESS')
+access_password=config('ACC_PASSWORD')
+reference=config('REFERENCES')
 
-
-client = Client(access_code=access_code, access_password=access_password, reference=reference)
+client = Client(access_code=access_code, access_password=access_password)
 balance = client.get_wallet_balance()
-logger.info(f'Wallet balance is {balance}')
+logger.success(f'wallet balance:{balance}')
+
+response = client.recharge_airtime(20,'0787442594')
+logger.success(f'Recharge status:{response}')
+
+
+data_bundles = client.get_data_bundles()
+logger.success(data_bundles.json())
